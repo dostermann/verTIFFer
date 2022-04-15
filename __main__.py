e222dbdd04c1237fbe63b13f796e04e847e93cff
dostermann <dostermann@mailbox.org>
2022-04-15 17:39:31 +0200

    put everything into a class

--- __main__.py
+++ __main__.py
@@ -1,3 +1,4 @@
+import sys
 import os
 from pdf2image import convert_from_path, convert_from_bytes
 
@@ -9,20 +10,35 @@ import customtkinter as ctk
 ctk.set_appearance_mode("System")
 ctk.set_default_color_theme("blue")
 
-tk_window = ctk.CTk()
-tk_window.geometry("512x256")
 
-def button_func():
-    pass
-button = ctk.CTkButton(master=tk_window, text="verTIFF mich!",
-                       command=button_func)
-button.place(relx=0.5, rely=0.5, anchor=tkinter.CENTER)
-
-tk_window.mainloop()
-
-
-class Vertiffer:
+class Vertiffer(ctk.CTk):
     """Main class for verTIFFer"""
 
+    WIDTH = 512
+    HEIGHT = 256
+
     def __init__(self):
+        super().__init__()
+
+        self.title("verTIFFer 0.1.0")
+        self.geometry(f"{Vertiffer.WIDTH}x{Vertiffer.HEIGHT}")
+
+        self.protocol("WM_DELETE_WINDOW", self.on_closing)
+
+        self.button = ctk.CTkButton(master=self, text="verTIFF mich!",
+                       command=self.button_func)
+        self.button.place(relx=0.5, rely=0.5, anchor=tkinter.CENTER)
+
+    def button_func(self):
         pass
+
+    def on_closing(self, event=0):
+        self.destroy()
+
+    def start(self):
+        self.mainloop()
+
+
+if __name__ == "__main__":
+    app = Vertiffer()
+    app.start()
